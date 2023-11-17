1d49ebca53f0ded86bf5e5aea2f9e52246528c45
Bruno Santos <brunohsantos1993@gmail.com>
2023-11-16 22:56:43 -0300

    code modification (alert)

--- java.js
+++ java.js
@@ -1,8 +1,22 @@
 const resultValue = document.getElementById('button');
 
 function toSpin() {
+    const enterInput = document.querySelector('.input-enter');
+    const thisInput = document.querySelector('.input-this');
+
+    // Verifica se ambos os números foram adicionados
+    if (!enterInput.value || !thisInput.value) {
+        alert("Please add both numbers.");
+        return; // Retorna imediatamente se a condição não for atendida
+    }
+    
     const enter = Math.ceil(parseInt(document.querySelector('.input-enter').value));
     const andThis = Math.floor(parseInt(document.querySelector('.input-this').value));
+
+    if (enter >= andThis) {
+        alert("The first number must be less than the second number.");
+        return; // Retorna imediatamente se a condição não for atendida
+    }
 
     let countdown = 10;
 
@@ -21,9 +35,15 @@ function toSpin() {
             const result = Math.floor(Math.random() * (andThis - enter + 1)) + enter;
             resultValue.innerHTML = `<span style="color: gold; font-weight: bold;">${result}</span>`;
 
-            // Muda a cor do texto para verde quando o resultado aparecer
-            resultValue.style.color = 'Blue';
+            // Muda a cor do texto para o dourado quando o resultado aparecer
+            resultValue.style.color = 'white';
+
+            setTimeout(function () {
+                // Exibe automaticamente um alerta "PARABÉNS" com o resultado
+                alert(`CONGRATULATIONS!`);
+            }, 500);
         }
+        
     }
 
     // Inicia a contagem regressiva quando a função é chamada
